2a91ca43dc059dc9bd7a0a5a0ee68f5cfc18072f
Sol-in-ctrl <arturaetus@gmail.com>
2020-05-09 00:41:38 -0300

    Novo arquivo de shell script

---
+++ Archpostinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Meu post-install do arch linux
+
+#Primeiro, atualizar os repositórios.
+
+sudo pacman -Syyu
+
+
+
+
+printf "Arch Linux, onde 30 minutos atrás é considerado estável\n"
+
+
