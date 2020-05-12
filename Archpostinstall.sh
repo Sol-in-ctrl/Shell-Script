f630ba0399d0f22323dd986f513b99d9850bb9a1
Sol-in-ctrl <arturaetus@gmail.com>
2020-05-11 21:41:25 -0300

    Instalando yay e snap

--- Archpostinstall.sh
+++ Archpostinstall.sh
@@ -2,9 +2,31 @@
 
 #Meu post-install do arch linux
 
-#Primeiro, atualizar os repositórios.
+#Primeiro, atualizar os repositórios e instalar atulizções se necessario.
 
-sudo pacman -Syyu
+echo"Verificando se à atualizações"
+sudo pacman -Syu
+
+#Instalando yay, AUR Helper.
+
+echo"Instalando o yay"
+cd /tmp
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+#instalando snapd.
+
+git clone https://aur.archlinux.org/snapd.git
+cd snapd
+makepkg -si
+
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+
+echo"Reinicie o computador para que os snaps funcionem"
+
+cd ~
 
 
 
